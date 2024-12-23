e497b380654deaab4a4db46ad6dd22007e137a27
Bisrat Kebere <kiyyakebere@gmail.com>
2024-12-23 04:04:54 +0300

    feat: create Livestock Schema

---
+++ livestockmanagement/src/services/livestock-service/models/Livestock.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const livestockSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    type: { type: String, enum: ["cow", "goat", "sheep"], required: true },
+    age: { type: Number, required: true },
+    healthStatus: { type: String, default: "healthy" },
+    collarId: { type: String, required: true, unique: true },
+    ownerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Livestock", livestockSchema);
